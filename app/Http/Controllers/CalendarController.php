387d8c45b4a43c28ecaea5e2c6aaf2f5defc7982
keiito <method03110201@gmail.com>
2021-06-08 21:04:58 +0900

    テスト

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,86 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public $prev;
+    public $next;
+    public $yearMonth;
+    private $_thisMonth;
+
+    public function __construct(){
+        try{
+            if(!isset($GET['t']) || !preg_match('\/A\d{4}-\d{2}\z/7',$GET['t'])){
+                throw new \Exception();
+            }
+            $this->_thisMonth=new \DateTime($_GET['t']);
+        }catch(\Exception $e){
+            $this->_thisMonth=new \DateTime('first day of this month');
+        }
+        $this->prev=$this->_ç();
+        $this->next=$this->_createNextLink();
+        $this->yearMonth = $this->_thisMonth->format('F Y');
+    }
+
+    private function _createPrevtLink(){
+        $dt=clone $this->_thisMonth;
+        return $dt->modify('-1 month')->format('Y-m');
+    }
+    private function _createNextLink(){
+        $dt=clone $this->_thisMonth;
+        return $dt->modify('+1 month')->format('Y-m');
+    }
+    public function show(){
+        $tail=$this->_getTail();
+        $body=$this->_getBody();
+        $head=$this->_getHead();
+        $html = '<tr>' . $tail . $body . $head . '</tr>';
+        print $html;
+    }
+    private function _getTail(){
+        $tail = '';
+        $lastDayOfPrevMonth = new \DateTime('last day of'.$this->yearMonth.'-1month');
+        while ($lastDayOfPrevMonth->format('w') < 6) {
+            $tail = sprintf('<td class="gray">%d</td>', $lastDayOfPrevMonth->format('d')) 
+            .$tail;
+            $lastDayOfPrevMonth->sub(new \DateInterval('P1D'));
+    }
+     return $tail;
+
+    }
+    private function _getBody(){
+        $body = '';
+        $period = new \DatePeriod(
+        new \DateTime('first day of' .$this->yearMonth),
+        new \DateInterval('P1D'),
+        new \DateTime('first day of' .$this->yearMonth.'+1 month')
+    );
+
+    $today= new DateTime('today');
+    foreach ($period as $day) {
+        if ($day->format('w')  ==='0') {$body .= '</tr><tr>';}
+        $todayClass=($day->format('Y-m-d')===$today->format('Y-m-d'))?'today':'';
+        $body .= sprintf('<td class="youbi_%d %s">%d</td>',$day->format('w'),
+        $todayClass,$day->format('d'));
+        }
+        return $body;
+    }
+    private function _getHead(){
+        $head = '';
+        $firstDayOfNextMonth = new \DateTime('first day of'.$this->yearMonth.'+1 month');
+        while ($firstDayOfNextMonth->format('w') > 0) {
+            $head .= sprintf('<td class="gray">%d</td>', $firstDayOfNextMonth->format('d'));
+            $firstDayOfNextMonth->add(new \DateInterval('P1D'));
+        }
+        return $head;
+
+    }
+
+}
+
+?>
+
---
+++ resources/views/calendar/Calendar_page.blade.php
@@ -0,0 +1,54 @@
+<?php
+
+require 'CalendarController.php';
+
+function h($s){
+    return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
+}
+
+$cal=App\Http\Controller\calendarController();
+
+?>
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>カレンダーページ</title>
+    <link rel="stylesheet" href="css/calendar.css">
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th><a href="/?t=<?php print h($cal->$prev);?>">&laquo;</a></th>
+                <th colspan="5"><?php print h($cal->$yearMonth);?></a></th>
+                <th><a href="/?t=<?php print h($cal->$next);?>">&raquo;</a></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sun</td>
+                <td>Mon</td>
+                <td>Tue</td>
+                <td>Wed</td>
+                <td>Thu</td>
+                <td>Fri</td>
+                <td>Sat</td>
+            </tr>
+
+            <?php $cal->show(); ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="7"><a href="/">Today</a></th>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
+</html>
+
