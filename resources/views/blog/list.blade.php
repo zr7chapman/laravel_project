24a83539846939da099d4a0f8b2ae574ca785e5a
keiito0719 <method03110201@gmail.com>
2021-06-12 15:47:08 +0000

    ブログ機能の編集、削除の設定

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-@section('title', '感情日記一覧')
+@section('title', 'ブログ一覧')
 @section('content')
 <!-- 
 ①route作成（削除ボタン）
@@ -8,7 +8,7 @@
  -->
 <div class="row">
     <div class="col-md-10 col-md-offset-2">
-        <h2>感情日記一覧</h2>
+        <h2>ブログ記事一覧</h2>
         @if (session('err_msg'))
             <p class="text-danger">
                 {{ session('err_msg') }}
@@ -16,32 +16,28 @@
         @endif
         <table class="table table-striped">
             <tr>
-                <th>日記番号</th>
+                <th>記事番号</th>
                 <th>タイトル</th>
-                <th>感じたこと</th>
-                <th>画像</th>
                 <th>日付</th>
                 <th></th>
                 <th></th>
             </tr>
             @foreach($blogs as $blog)
-            <!-- <tr>
+            <tr>
                 <td>{{ $blog->id  }}</td>
                 <td><a href="/blog/{{ $blog->id }}">{{ $blog->title  }}</a></td>
-                <td></td>
-                <td></td>
                 <td>{{ $blog->updated_at  }}</td>
                 <td><button type="button" class="btn btn-primary" onclick="location.href='/blog/edit/{{ $blog->id }}'">編集</button></td>
                 <form method="POST" action="{{ route('delete', $blog->id) }}" onSubmit="return checkDelete()">
                 {{ csrf_field() }}
                 <td><button type="submit" class="btn btn-primary" onclick=>削除</button></td>
                 </form>
-            </tr> -->
+            </tr>
             @endforeach
         </table>
     </div>
 </div>
-<!-- <script>
+<script>
 function checkDelete(){
     if(window.confirm('削除してよろしいですか？')){
         return true;
@@ -49,5 +45,5 @@ function checkDelete(){
         return false;
     }
 }
-</script> -->
+</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,21 @@
 */
 // ブログ一覧画面を表示
 Route::get('/', 'BlogController@showList')->name('blogs');
+// ブログ登録画面を表示
+Route::get('/blog/create', 'BlogController@showCreate')->name('create');
+
+// ブログを登録
+Route::post('/blog/store', 'BlogController@exeStore')->name('store');
+
 // ブログ詳細画面を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
+
+// ブログ編集画面を表示
+Route::get('/blog/edit/{id}', 'BlogController@showEdit')->name('edit');
+Route::post('/blog/update', 'BlogController@exeUpdate')->name('update');
+
+// ブログ削除
+Route::post('/blog/delete/{id}', 'BlogController@exeDelete')->name('delete');
     
 
 Route::get('/messages', 'MessagesController@index');
